908396c6d6203b543061095e1adb669d87ac3770
zonyamb <zonyam022@student.wethinkcode.co.za>
2023-11-04 09:46:34 -0500

    refactored database code to oop --rickeddy

--- webapp/database.py
+++ webapp/database.py
@@ -1,18 +1,13 @@
-
 import sqlite3
 
-
-
-def connect_to_database():
-    '''established a connection to the literacy lab database'''
-
-    return sqlite3.connect("literacy_lab.db")
-
-
-def create_tables(cursor: sqlite3.Cursor):
-    '''creates the schema(s) of the database'''
-
-    students = """CREATE TABLE students(
+class LiteracyLabDatabase:
+    
+    def __init__(self, db_file):
+        self.conn = sqlite3.connect(db_file)
+        self.cursor = self.conn.cursor()
+    
+    def create_tables(self):
+        students = """CREATE TABLE students(
             student_id INTEGER PRIMARY KEY AUTOINCREMENT,
             first_name TEXT(25),
             surname TEXT(25),
@@ -21,18 +16,16 @@ def create_tables(cursor: sqlite3.Cursor):
             grade TEXT(2),
             parent_id INTEGER,
             FOREIGN KEY(parent_id) REFERENCES parents
-        );
-    """
-    educators = """CREATE TABLE educators(
+        );"""
+        educators = """CREATE TABLE teachers(
             educator_id INTEGER PRIMARY KEY AUTOINCREMENT,
             first_name TEXT(25),
             surname TEXT(25),
             id_number INTEGER,
             password INTEGER,
-            dicsipline TEXT(30)
-        );
-    """
-    parents = """CREATE TABLE parents(
+            discipline TEXT(30)
+        );"""
+        parents = """CREATE TABLE parents(
             parent_id INTEGER PRIMARY KEY AUTOINCREMENT,
             first_name TEXT(25),
             surname TEXT(25),
@@ -40,36 +33,36 @@ def create_tables(cursor: sqlite3.Cursor):
             phone_number TEXT(10),
             child_id INTEGER,
             FOREIGN KEY(child_id) REFERENCES students
-        );
-    """
+        );"""
+        
+        self.cursor.execute(students)
+        self.cursor.execute(educators)
+        self.cursor.execute(parents)
+        self.conn.commit()
     
-    cursor.execute(students)
-    cursor.commit()
-    cursor.execute(educators)
-    cursor.commit()
-    cursor.execute(parents)
-    cursor.commit()
+
+    def add_student(self, values):
+        insert = """INSERT INTO students (first_name, surname, id_number, password, grade, parent_id) VALUES (?, ?, ?, ?, ?, ?);"""
+        self.cursor.execute(insert, values)
+        self.conn.commit()
 
 
-def add_student(cursor: sqlite3.Cursor, values: list):
-    '''adds a students' credentials to the database'''
-
-    insert = """INSERT INTO students (first_name, surname, id_number, password, grade, parent_id) VALUES (?, ?, ?, ?, ?, ?);"""
+    def add_educator(self, values):
+        insert = """INSERT INTO teachers (first_name, surname, id_number, password, discipline) VALUES (?, ?, ?, ?, ?);"""
+        self.cursor.execute(insert, values)
+        self.conn.commit()
     
-    cursor.execute(insert, values).commit()
 
-
-def add_educator(cursor: sqlite3.Cursor, values: list):
-    '''adds a students' credentials to the database'''
-
-    insert = """INSERT INTO educators (first_name, surname, id_number, password, dicsipline) VALUES (?, ?, ?, ?, ?);"""
+    def add_parent(self, values):
+        insert = """INSERT INTO parents (first_name, surname, id_number, phone_number, child_id) VALUES (?, ?, ?, ?, ?);"""
+        self.cursor.execute(insert, values)
+        self.conn.commit()
     
-    cursor.execute(insert, values).commit()
 
-
-def add_parent(cursor: sqlite3.Cursor, values: list):
-    '''adds a students' credentials to the database'''
-
-    insert = """INSERT INTO parents (first_name, surname, id_number, phone_number, child_id) VALUES (?, ?, ?, ?, ?);"""
+    def validate_student(self, values):
+        select = """SELECT * FROM students WHERE id_number = ? AND first_name = ?"""
+        return self.cursor.execute(select, values).fetchall()
     
-    cursor.execute(insert, values).commit()
+   
+    def close_connection(self):
+        self.conn.close()
