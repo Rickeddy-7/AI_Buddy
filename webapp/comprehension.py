ca3b0d292147957392bc639daa50a841e39d479e
sampofu022 <sampofu022@student.wethinkcode.co.za>
2023-11-05 06:07:46 +0200

    added comprehension

---
+++ webapp/comprehension.py
@@ -0,0 +1,65 @@
+
+from gpt_models import use_gpt
+import random
+
+questions = [
+    "What did Lily find in the woods near her house?",
+    "What happened when Lily touched the leaves of the tree?",
+    "What creatures did Lily befriend in the woods?",
+    "What did Lily have to do to return to her normal size?",
+    "How did Lily feel after she grew back to her natural size?",
+    "What kind of creature was on its way to becoming a butterfly?",
+    "Why did Lily have to leave her new friends in the woods?",
+    "Who showed Lily how to reverse the spell and grow back to her normal size?",
+    "What did Lily learn from her experience in the woods?",
+    "What was the name of the magical tree that Lily had to find?"
+]
+
+answers = [
+    "She found a magical tree with shimmering leaves.",
+    "She shrunk down to the size of a ladybug.",
+    "She became friends with ants and a caterpillar.",
+    "She had to eat a berry from the tree of life.",
+    "She had a newfound appreciation for the world around her.",
+    "A caterpillar.",
+    "She knew she had to find a way back to her normal size.",
+    "A fairy who lived in the woods.",
+    "She learned to appreciate the world around her and the creatures that live in it.",
+    "The tree of life."
+]
+
+affirmations = ["Correct, keep going!", "Nice one!", "Excellent!", "You're on fire!"]
+
+def play_game():
+    '''one of the features under the literacy lab'''
+
+    role = """you are tasked with checking wether two statments are the same or not. If they are the similar, respond with just 'true'
+        else 'false'. Ignore the case and focus on the gist of the statement"""
+    
+    print("Read the following story and answer the questions that follow:\n\n")
+    read_story_from_file()
+    
+    while(True):
+        for i in range(len(questions)):
+            user_ans = input(questions[i])
+            prompt = f"Is '{user_ans}' similar to '{answers[i]}' ?'"
+            result: str = use_gpt(role, prompt)
+            print(result.upper())
+            while not (result.lower() == 'true'):
+                print("Not quite, try again\n")
+                user_ans = input(questions[i])
+                prompt = f"Is '{user_ans}' similar to '{answers[i]}' ?'"
+                result: str = use_gpt(role, prompt)
+            else:
+                print(random.choice(affirmations) + '\n')
+                continue
+
+
+def read_story_from_file():
+    '''opens the txt files that contains the comprehension text'''
+
+    with open('webapp/story.txt') as file:
+        print(file.readlines())
+
+
+play_game()
--- webapp/gpt_models.py
+++ webapp/gpt_models.py
@@ -7,7 +7,7 @@ openai.api_key = "37635d8c1d7349fe8698c3d77f855d2e"
 openai.api_base = "https://literacylab.openai.azure.com/"
 
 
-def use_general_gpt(role: str, prompt: str):
+def use_gpt(role: str, prompt: str):
     '''the general version of the model where the user gives personal prompts and the
         model responds as instructed(specified by role)'''
     
@@ -20,3 +20,9 @@ def use_general_gpt(role: str, prompt: str):
     )
     return response['choices'][0]['message']['content']
 
+
+role = "you are a grade four english teacher who is friendly and helpful"
+prompt = """generate a fun and interesting story of about 4/5 paragraphs for the student and thereafter
+            generate ten quesions along with their answers to test their understanding."""
+# response = use_gpt(role, prompt)
+# print(f'GPT >> {response}')
